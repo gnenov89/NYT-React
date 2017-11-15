83c73000a6d3f4be3f51c8ac4d6effae3d481975
gnenov89 <georgi.nenov89@gmail.com>
2017-11-15 14:34:51 -0500

    models added

---
+++ nyt/models/Article.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose");
+
+// create a schema classs 
+var Schema = mongoose.Schema;
+
+// create article Schema
+var ArticleSchema = new Schema ({
+	title:{
+		type: String,
+		required: true
+	},
+	link: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		date: Date
+	}
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model('Article', ArticleSchema);
+
+module.exports = Article;
